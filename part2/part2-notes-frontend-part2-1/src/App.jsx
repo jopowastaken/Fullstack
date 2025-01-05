bbcf1288e5e59e7bed30da8869a17b3826213037
jopowastaken <148993219+jopowastaken@users.noreply.github.com>
2025-01-05 20:37:52 +0200

    setit

--- part2/part2-notes-frontend-part2-1/src/App.jsx
+++ part2/part2-notes-frontend-part2-1/src/App.jsx
@@ -1,49 +1,14 @@
-import { useState } from 'react'
+import Note from './components/Note'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-
-  }
-
-  const addPerson = (event) => {
-    event.preventDefault()
-    for (let person of persons) {
-      if (person.name === newName) {
-        alert(`${newName} is already added to phonebook`)
-        setNewName('')
-        return
-      }}
-    console.log('button clicked', event.target)
-    const nameObeject = {
-      name: newName
-    }
-    setPersons(persons.concat(nameObeject))
-    setNewName('')
-    console.log('persons', persons)
-    }
-
+const App = ({ notes }) => {
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange = {handleNameChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map(persons => 
-        <div key={persons.name}>{persons.name}</div>
-      )}
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
   )
 }
